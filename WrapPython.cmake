5c6882a3abb308af064382cd5deadc3847876bf0
jmsgrogan <grogan@maths.ox.ac.uk>
2016-10-25 08:32:58 +0100

    Vtk 5 compatibility for a test.

--- WrapPython.cmake
+++ WrapPython.cmake
@@ -29,7 +29,7 @@
 # LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
 # OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 # 
-
+add_compile_options(-Wno-unused-local-typedefs)
 # Build the Python bindings
 add_definitions(-DCHASTE_MicrovesselChaste_PYTHON)
 find_package(Boost COMPONENTS python REQUIRED)
