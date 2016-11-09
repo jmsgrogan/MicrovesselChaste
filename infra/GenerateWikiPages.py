5544fc8a5b5e296310e23a3d2314e0bc0da7c816
jmsgrogan <grogan@maths.ox.ac.uk>
2016-11-09 08:27:35 +0000

    Include tutorials in auto-wiki generation.

--- infra/GenerateWikiPages.py
+++ infra/GenerateWikiPages.py
@@ -44,16 +44,20 @@ import ntpath
 if __name__ == '__main__':
 
     # Find all the tutorial files.
+    search_paths = ["test/tutorials/", "test/python/tutorials/"]
+    
     tutorial_files = []
-    for root, dirs, files in os.walk("test/tutorials/"):
-        for file in files:
-            if fnmatch.fnmatch(file, 'Test*LiteratePaper*'):
-                 tutorial_files.append(file)
-
+    for eachSearchPath in search_paths:
+        for root, dirs, files in os.walk(eachSearchPath):
+            for file in files:
+                if fnmatch.fnmatch(file, 'Test*LiteratePaper*') or fnmatch.fnmatch(file, 'Test*Tutorial*'):
+                    if not fnmatch.fnmatch(file, '*.pyc'):
+                        tutorial_files.append([eachSearchPath, file])
+                     
     # Generate the markdown for each and send it to the wiki repo.
     for eachFile in tutorial_files:
-        outfile = " doc/wiki/" + os.path.splitext(ntpath.basename(eachFile))[0] +".md"
-        inputfile = "test/tutorials/" + eachFile
+        outfile = " doc/wiki/" + os.path.splitext(ntpath.basename(eachFile[1]))[0] +".md"
+        inputfile = eachFile[0] + eachFile[1]
         launch_string = "infra/CreateMarkdownTutorial.py " + inputfile + outfile 
         os.system(launch_string)
 
