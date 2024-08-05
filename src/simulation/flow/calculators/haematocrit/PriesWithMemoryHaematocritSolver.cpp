93813516cbca50394e6ad5c01cf23903180008aa
Joe Pitt-Francis <Joe.Pitt-Francis@cs.ox.ac.uk>
2024-08-05 12:58:06 +0100

    Be verbose when preferred branch cannot be calculated

--- src/simulation/flow/calculators/haematocrit/PriesWithMemoryHaematocritSolver.cpp
+++ src/simulation/flow/calculators/haematocrit/PriesWithMemoryHaematocritSolver.cpp
@@ -274,10 +274,6 @@ void PriesWithMemoryHaematocritSolver<DIM>::CalculateVesselPreferences(std::vect
     // Sweep through looking for left and right turns
     for (unsigned idx=0; idx<updateIndices.size(); idx++)
     {
-        if (updateIndices[idx][0] == 1)
-        {
-            PRINT_3_VARIABLES(updateIndices[idx][0],updateIndices[idx][1],updateIndices[idx][2]);
-        }
         std::shared_ptr<Vessel<DIM> > me = vessels[updateIndices[idx][0]];
         std::shared_ptr<Vessel<DIM> > parent = vessels[updateIndices[idx][1]];
         std::shared_ptr<Vessel<DIM> > competitor = vessels[updateIndices[idx][2]];
